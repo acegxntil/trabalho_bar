81ed3a767542080f9a35730abcc06ccfa4084e5f
Daniel Gentil <danielgentil2003@gmail.com>
2020-05-07 16:20:36 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main() {
+  string sexo;
+
+  int cerveja , refri , espetos;
+
+  cout << "Sexo: ";
+  cin >> sexo;
+
+  cout << "Quantidade de cervejas: ";
+  cin >> cerveja;
+
+  cout << "Quantidade de refrigerantes: ";
+  cin >> refri;
+
+  cout << "Quantidade de espetinhos: ";
+  cin >> espetos;
 
 
   return 0;
-}
+}s
